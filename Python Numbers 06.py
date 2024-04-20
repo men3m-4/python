74ab344b8f88fe614925e062affd40f26177e18e
men3m <mamree6@gmail.com>
2024-04-20 19:51:37 -0400

    06

---
+++ Python Numbers 06.py
@@ -0,0 +1,20 @@
+print(2)
+print(2+2)
+print(2+2*3)         #the priority for *   the result is 8
+print((2+2)*3)       #the result is 12
+print(5%3)           #divide the number and give the remainder in output  2
+my_num=5
+print(str(my_num))
+print(str(my_num)+"  is the best number") # if you didnt write str will give you error
+print(abs(my_num)) # give the absolute for you number
+print(pow(2,3))      #2^3
+print(max(2,9))      #give the maximum number
+print(min(2,9))      #give the manimum number
+print(round(2.9))    #  ~~3
+print(round(2.3))    #  ~~2
+from math import *   #use all functions in the math lib
+print(ceil(2.9))     #~~3
+print(ceil(2.1))     #~~3
+print(floor(2.9))    #~~2
+print(floor(2.1))    #~~2
+print(sqrt(16))      #gvive you the square root of the number
