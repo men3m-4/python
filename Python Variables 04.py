f84fb8fa7801d46f799b209193cdb366e8a93c16
men3m <mamree6@gmail.com>
2024-04-20 18:27:33 -0400

    python variables 04

---
+++ Python Variables 04.py
@@ -0,0 +1,13 @@
+character_name = "men3m"
+age = "23"
+is_male = True
+print("There is a man called " + character_name )
+print("His age is " + age)
+print(character_name + " is Real Madrid big fan")
+print("His favorite player is Cristiano Ronaldo")
+
+character_name = "cr7"
+age = "39"
+print("He is goat")
+print("Cristiano Ronaldo age is " + age)
+print("Thank you for everything " + character_name)
