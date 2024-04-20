9d02b9c9f35d0118ae3a0f8224f3df28409369a1
men3m <mamree6@gmail.com>
2024-04-20 19:20:59 -0400

    05

---
+++ Python Strings 05.py
@@ -0,0 +1,9 @@
+text="men3m"
+print(text.upper())   #if you need capital use this  the output will be MEN3M
+print(text.lower())   #if you need capital use this  the output will be men3m
+print(text.isupper()) #if it was capital the output will be true
+print(text.islower()) #if it was small the output will be true
+print(len(text))      #len ia abbreviation of lenth thr number of men3m =5
+print(text[2])        #give the location of the letter  m=0 e=1 n=2 3=3 m=4 ....
+print(text.index("n"))#give the location of the letter if you write value in the index not exist will give you error
+print(text.replace("men","abdelmon")) #replace the old value  by the new value will be  abdelmon3m
