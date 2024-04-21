4d12118cd186eb276a1dbe8caeaf98a8819f0f40
men3m <mamree6@gmail.com>
2024-04-20 20:17:15 -0400

    08

---
+++ Build Calculator in Python 08.py
@@ -0,0 +1,8 @@
+##num1 = input("enter the first number")
+#num2 = input("enter the second number")
+#result=int(num1)+int(num2)
+#print(result)
+num1 = input("enter the first number")
+num2 = input("enter the second number")
+result=float(num1)+float(num2)
+print(result)
