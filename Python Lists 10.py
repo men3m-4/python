911412285d8d743e440fe491d94a0a51ac613fa7
men3m <mamree6@gmail.com>
2024-04-22 19:44:23 -0400

    10

---
+++ Python Lists 10.py
@@ -0,0 +1,11 @@
+#frends=[1,"men3m",True,False,[1,"m"]]
+#print(frends[2])
+frends=["codezella","men3m","python","pro","zz"]
+print(frends[-1])      # the output from the end
+
+print(frends[1:3])     #print 1 and 2
+print(frends[1:4])     #print 1,2,3    dint include 4
+print(frends[1:])      #print from  1 to the end
+print(frends[2:])      #print from  2 to the end
+frends[0]="hh"         #change the value in 0
+print(frends[0])       #the output will change to the new hh
