6d2c18e6bca886e798d054d31201894392ccc700
men3m <mamree6@gmail.com>
2024-04-25 22:42:42 -0400

    12

---
+++ Python Tuples 12.py
@@ -0,0 +1,5 @@
+coordinates=(23,45)
+print(coordinates)
+print(coordinates[1])             # print the value in the location 1     0,1,2,3,4,5,6,......
+#coordinates[0]=66       TypeError: 'tuple' object does not support item assignment
+list_of_tuples=[(1,2),(1,3),(4,4)]
