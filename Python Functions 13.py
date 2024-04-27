3c4cd8b80f966b67f7ea20641829cbf500792fe4
men3m <mamree6@gmail.com>
2024-04-26 20:15:16 -0400

    12

---
+++ Python Functions 13.py
@@ -0,0 +1,8 @@
+def say_hi(name,age):
+    #def say_hi(name,age ,True, False ,[1,"string"],(12,14)):   any data type
+
+    print("hello " +name+ " your age is " + str(age))
+
+
+say_hi("men3m", 22)                           #call the function
+
